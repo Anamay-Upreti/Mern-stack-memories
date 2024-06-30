27ec9e79ff61a1d47c6ed7ee211d6f747642e534
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-06-30 15:55:55 +0530

    added-postMEssage

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,17 @@
 import express from "express";
 
+import { getPosts } from "../controllers/posts.js";
+
 
 const router = express.Router();
 
-// lh 5000;/posts//
 
-router.get('/', (req, res) => {
-       res.send('This Works')
-})
-
+router.get('/', getPosts);
+router.get('/', getPosts);
+router.get('/', getPosts);
+router.get('/', getPosts);
+router.get('/', getPosts);
+router.get('/', getPosts);
+       
 
 export default router
